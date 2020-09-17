f707df7a310aa08169546c907c1f64c552dd83df
sajithl <70686084+sajithl@users.noreply.github.com>
2020-09-17 19:02:40 +0530

    Add files via upload

---
+++ DataModel/CreditLimits.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DataModel
+{
+    public class CreditLimits
+    {
+        public string Entity { get; set; }
+        public string Parent { get; set; }
+        public int CreditLimit { get; set; }
+        public int Utilization { get; set; }
+
+        public static CreditLimits FromCsv(string csvLine)
+        {
+            string[] values = csvLine.Split(',');
+            CreditLimits creditValues = new CreditLimits
+            {
+                Entity = Convert.ToString(values[0].Trim()),
+                Parent = Convert.ToString(values[1].Trim()),
+                CreditLimit = Convert.ToInt32(values[2].Trim()),
+                Utilization = Convert.ToInt32(values[3].Trim())
+            };
+            return creditValues;
+        }
+    }
+}
